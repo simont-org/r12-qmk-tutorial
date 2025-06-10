0eebde53e1099becac56444c29ddaada4451329e
simont-org <68000215+simont-org@users.noreply.github.com>
2025-06-10 12:11:39 +0200

    Update keymap.c

--- keyboards/r12_simont/keymaps/default/keymap.c
+++ keyboards/r12_simont/keymaps/default/keymap.c
@@ -5,9 +5,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_H      ,KC_F      ,KC_E      ,KC_ESCAPE , S(KC_1),   
         KC_X      ,KC_M      ,KC_D      ,KC_I      ,
         KC_1      ,KC_2      ,KC_3      ,KC_ENT    
-            
+        
     )
 };
+
+// function per simulare la rotella del mouse quando si ruota l'encoder
 
 bool encoder_update_user(uint8_t i, bool cw) {
     tap_code16(cw ? KC_WH_D : KC_WH_U);   // scroll down / up
